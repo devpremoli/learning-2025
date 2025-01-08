82828bd579211d4a35b708adf819d141e42ba47c
devpremoli <prem.oli@bison.howard.edu>
2025-01-08 00:12:20 -0600

    react hooks

---
+++ react/hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UseEffect from "./use-effect/UseEffect";
+import UseRef from "./use-ref/UseRef"
+
+function App() {
+  return (
+    <>
+      {/* <UseEffect /> */}
+      <UseRef />
+    </>
+  );
+}
+
+export default App;
---
+++ react/hooks/src/use-ref/UseRef.jsx
@@ -0,0 +1,14 @@
+import CountUseRef from "./CountUseRef";
+import InputUseRef from "./InputUseRef";
+
+function UseRef() {
+  return (
+    <>
+      <CountUseRef />
+      <br />
+      <InputUseRef />
+    </>
+  );
+}
+
+export default UseRef;
